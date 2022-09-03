6cae6df44fee2f176eba7106890d62ed7798ff8d
freetimezp <kraftukrnet@ukr.net>
2022-09-03 14:32:18 +0300

    05. js-e-commerce-01. Connect to Sanity. Display banner data from Sanity.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import {client} from "../lib/client";
 import {Product, FooterBanner, HeroBanner} from "../components";
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
     return (
         <>
-            <HeroBanner />
+            <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+
 
             <div className="products-heading">
                 <h2>Best Selling Products</h2>
@@ -14,13 +17,25 @@ const Home = () => {
 
             <div className="products-container">
                 {
-                    ["Product 1", "Product 2"].map((product) => <Product />)
+                    products?.map((product) => product.name)
                 }
             </div>
 
             <FooterBanner />
         </>
     )
+}
+
+export const getServerSideProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+        props: {products, bannerData}
+    }
 }
 
 export default Home;
