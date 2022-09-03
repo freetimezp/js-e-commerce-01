ebd51603b17cfeb2d725d70984d786b46db77c21
freetimezp <kraftukrnet@ukr.net>
2022-09-03 15:35:06 +0300

    06. js-e-commerce-01. Product component. Display data.

--- sanity-ecommerce-01/schemas/product.js
+++ sanity-ecommerce-01/schemas/product.js
@@ -19,7 +19,7 @@ export default {
         },
         {
             name: 'slug',
-            title: 'Name',
+            title: 'Slug',
             type: 'slug',
             options: {
                 source: 'name',
